6f03046d3b5cf6263d2bf380751a9ea4c12b8e30
michaelmichaeli888 <michaelmichaeli888@gmail.com>
2020-08-27 16:20:13 +0300

    27-08-2020-B

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -3,66 +3,17 @@ import React, { Component } from 'react'
 import { saveUser } from '../actions/UserActions.js';
 import { connect } from 'react-redux';
 
-
 class ProjectPreview extends Component {
 
     state = {
         isInEditMode: false,
         projectName: ''
     }
+
     componentDidMount() {
         const { props } = this
         this.setState(prevState => ({ ...prevState, projectName: props.project.name }))
     }
-    handleChange = (ev) => {
-        const { name, value } = ev.target;
-        this.setState({ [name]: value })
-    }
-
-    // handleSubmit = (ev) => {
-    //     const userToSave = JSON.parse(JSON.stringify(this.props.loggedInUser));
-    //     ev.preventDefault();
-    //     let project = {}
-    //     project.name = this.state.project;
-    //     project.id = makeId();
-    //     userToSave.projects.unshift(project);
-    //     console.log('project on submit:', project);
-    //     console.log('handle submit add project:', this.state);
-    //     this.props.saveUser(userToSave);
-    //     this.setState({ project: '' });
-    // }
-
-
-
-    // handleChange = ({ target }, idx = -1) => {
-    //     const field = target.name;
-    //     const value = (field === 'isDone') ? target.checked : target.value;
-    //     let cloneChkList = this.state.checkList.slice();
-    //     if (field === 'isDone') {
-    //         cloneChkList[idx].isDone = value;
-    //         this.setState({ checkList: cloneChkList }, () => {
-    //             this.handleSaveBoard();
-    //         });
-    //     }
-    //     else {
-    //         if (idx === -1)
-    //             (this.state.onAdd ? this.setState({ todoText: value }) : this.setState({ checklistTitle: value }))
-
-    //         else {
-
-    //             cloneChkList[idx].txt = value;
-    //         }
-    //     }
-    //     this.setState({ checkList: cloneChkList });
-    // }
-
-    // handleFocus = (ev) => ev.target.select();
-
-    // handleKeyPress(e) {
-    //     if (e.keyCode === 13) {
-    //         e.target.blur();
-    //     }
-    // }
 
     handleProjectClicked() {
         this.setState({ isInEditMode: true })
@@ -75,10 +26,8 @@ class ProjectPreview extends Component {
         this.setState({ isInEditMode: false })
     }
     handleProjectChange(ev) {
-        this.setState({ projectName : ev.target.value })
-
+        this.setState({ projectName: ev.target.value })
     }
-
 
     render() {
         return (
@@ -86,8 +35,8 @@ class ProjectPreview extends Component {
                 {this.state.isInEditMode ? <input type="text"
                     defaultValue={this.state.projectName}
                     autoFocus
-                onChange={(ev)=>this.handleProjectChange(ev)}
-                onBlur={() => this.handleProjectBlured()}
+                    onChange={(ev) => this.handleProjectChange(ev)}
+                    onBlur={() => this.handleProjectBlured()}
 
                 />
                     : <p onClick={() => this.handleProjectClicked()}>{this.props.project.name}</p>
@@ -96,7 +45,6 @@ class ProjectPreview extends Component {
         )
     }
 }
-
 
 const mapStateToProps = (state) => {
     return {
@@ -107,7 +55,5 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
     saveUser
 }
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectPreview)
